1291281a21b1d7ed90bda1e8c8880c2b012dd652
Zazulei <abelvj@gmail.com>
2016-06-04 01:22:20 +0200

    Added player to scene test

---
+++ IsGood/Assets/Script/InputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	public Transform tPlayer;
+	public int speed;
+	public int idPlayer;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(" Hola mundo ");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int moveX = (int) Input.GetAxis ("MoveX_P" + idPlayer); 
+		int moveY = (int) Input.GetAxis ("MoveY_P" + idPlayer); 
+		Debug.Log( moveX + " - " + moveY );
+
+		Vector3 position = tPlayer.position;
+		position.x += speed * Time.deltaTime * moveX;
+		position.y += speed * Time.deltaTime * moveY;
+
+		tPlayer.position = position;
+	}
+}
