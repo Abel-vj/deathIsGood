dbbd4592cb8deb8a70754816726cf379946dde25
Juan Garnica <jupegarnica@gmail.com>
2016-06-05 00:59:28 +0200

    HUD

---
+++ IsGood/Assets/Script/HUD.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	public Texture p1;
+	public Texture p2;
+	public Texture p3;
+	public Texture p4;
+
+	private int totalPlayers = 4;
+
+	// Use this for initialization
+	void Start () {
+
+		//totalPlayers = Input.GetJoystickNames().Length;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if (totalPlayers >= 1) {
+			GUI.Label (new Rect (10, 0, 200, 200), p1);	
+		}
+		if (totalPlayers >= 2) {
+			GUI.Label (new Rect (10, 100, 200, 200), p2);
+		}
+		if (totalPlayers >= 3) {
+			GUI.Label (new Rect (Camera.current.pixelWidth - 100, 0, 200, 200), p3);
+		}
+		if (totalPlayers >= 4) {
+			GUI.Label (new Rect (Camera.current.pixelWidth - 100, 100, 200, 200), p4);
+		}
+
+
+
+	}
+
+}
